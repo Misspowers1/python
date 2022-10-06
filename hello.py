454db12d9399b32e480d7b60cdbd670d4ac36e37
misspowers1 <misspowers01@gmail.com>
2022-10-06 07:58:34 +0300

    variables

--- hello.py
+++ hello.py
@@ -1,2 +1,6 @@
-msg = "Hello wworld"
-print(msg)
+"""Naming and using variables"""
+#msg = "Hello wworld"
+# print(msg)
+
+#simple_message = "I'm enjoying this learning process"
+# print(simple_message)
